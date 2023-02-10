86c521b49d483a7b27e5353eb80b78d647fe90f5
apton-anandhu-or <anandhu.or@aptonmworks.com>
2023-02-10 17:12:21 +0530

    Added pricing section

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,8 @@ import featureBgBtm from '../assets/images/feature/featureBgBtm.svg'
 import apiAnimation from '../assets/images/api/apiAnimation.svg'
 import CountUp from 'react-countup'
 import { useInView } from 'react-intersection-observer'
-
+import pricingTop from '../assets/images/pricing/pricingTop.svg'
+import pricingBtm from '../assets/images/pricing/pricingBtm.svg'
 
 export default function Home() {
 
@@ -33,7 +34,7 @@ export default function Home() {
 
       <main className={styles.main}>
 
-        {/* Navbar */}
+        {/* Navbar starts */}
 
         <nav className={`navbar navbar-expand-lg bg-body-tertiary ${styles.customnavbar}`}>
           <div className={`container ${styles.navContainer}`}>
@@ -65,7 +66,9 @@ export default function Home() {
           </div>
         </nav>
 
-        {/* Hero */}
+        {/* navbar Ends */}
+
+        {/* Hero starts */}
 
         <div className={styles.hero}>
           <div class="container text-center">
@@ -82,9 +85,11 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Features */}
+        {/* Hero ends */}
 
-        <div className={styles.features} id="features"> 
+        {/* Features starts */}
+
+        <div className={styles.features} id="features">
           <Image src={featureBgTop} width='1920' height='301' className={styles.featureBg} alt='feature bgTop' />
           <div class="container text-center">
             <div class="row">
@@ -110,7 +115,7 @@ export default function Home() {
 
                     <div className="col-sm">
                       <div className={styles.ftSubContent}>
-                        <Image src={ftIcon2} width='30' height='30'alt='feature icon' />
+                        <Image src={ftIcon2} width='30' height='30' alt='feature icon' />
                         <h2>24/7 Assistance</h2>
                         <p>We can handle a variety of tasks, such as routing calls, providing information,
                           and completing transactions, 24/7 without the need for human intervention.</p>
@@ -146,7 +151,9 @@ export default function Home() {
           <Image src={featureBgBtm} width='1920' height='301' className={styles.featureBgBtm} alt='featureBg btm' />
         </div>
 
-        {/* API */}
+        {/* Features ends */}
+
+        {/* API starts */}
 
         <div className={styles.api}>
           <div class="container text-center">
@@ -197,6 +204,81 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </div>
+
+        {/* Api ends */}
+
+        {/* Pricing Starts */}
+
+        <div className={styles.pricing}>
+          <Image src={pricingTop} width='1920' height='301' className={styles.featureBg} alt='feature bgTop' />
+          <div className={`container ${styles.pricingContainer}`}>
+            <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+              <h1 class={`display-4 fw-normal ${styles.pricingHeading}`}>Pricing</h1>
+              <p class={`fs-5 text-muted ${styles.pricingSubHeading}`}>Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
+            </div>
+
+            <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+              <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                  <div class="card-header py-3">
+                    <h4 class="my-0 fw-normal">Free</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>10 users included</li>
+                      <li>1 call bot</li>
+                      <li>2 chat bot</li>
+                      <li>2 GB of storage</li>
+                      <li>Email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button type="button" class={`w-100 btn ${styles.customBtn} ${styles.btn}`}>Sign up for free</button>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                  <div class="card-header py-3">
+                    <h4 class="my-0 fw-normal">Pro</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>20 users included</li>
+                      <li>2 call bot</li>
+                      <li>5 chat bot</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button type="button" class={`w-100 btn ${styles.customBtn} ${styles.btn}`}>Get started</button>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm border-primary">
+                  <div class={`card-header py-3 border-primary ${styles.priceCardActive}`}>
+                    <h4 class={`my-0 fw-normal`}>Enterprise</h4>
+                  </div>
+                  <div class="card-body">
+                    <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
+                    <ul class="list-unstyled mt-3 mb-4">
+                      <li>30 users included</li>
+                      <li>unlimited call bot</li>
+                      <li>unlimited chat bot</li>
+                      <li>15 GB of storage</li>
+                      <li>Phone and email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button type="button" class={`w-100 btn ${styles.customBtn} ${styles.btn}`}>Contact us</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Image src={pricingBtm} width='1920' height='301' className={styles.featureBg} alt='feature bgTop' />
         </div>
 
       </main>
