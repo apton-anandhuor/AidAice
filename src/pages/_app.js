1ccc9d0af043a0ebbbc1a293c968a0b025e342da
apton-anandhu-or <anandhu.or@aptonmworks.com>
2023-02-10 10:53:41 +0530

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
+// custom CSS
 import '@/styles/globals.css'
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      import("bootstrap/dist/js/bootstrap");
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
