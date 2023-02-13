8182a2ad0b929657b06984c7c12240bdddd0b21e
apton-anandhu-or <anandhu.or@aptonmworks.com>
2023-02-13 16:15:35 +0530

    Added Footer and Client Section

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const webpackConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }));
+
+    // Important: return the modified config
+    return config;
+  }
+};
+
+module.exports = { ...nextConfig, ...webpackConfig };
